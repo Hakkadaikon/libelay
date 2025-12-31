e36b09cf8c166b6e32281a575394ca226fa38b8b
hakkadaikon <hakkadaikon@yahoo.co.jp>
2025-12-31 22:04:32 +0900

    refactor: Organize nostr event validation

--- src/nostr/nostr_func.c
+++ src/nostr/nostr_func.c
@@ -49,74 +49,8 @@ bool json_to_nostr_event(const char* json, PNostrEvent event)
     return false;
   }
 
-  for (int i = 3; i < token_count; i += 2) {
-    int key_index   = i;
-    int value_index = i + 1;
-
-    if (!funcs.is_string(&token[key_index])) {
-      log_debug("JSON error: key is not string\n");
-      return false;
-    }
-
-    if (funcs.strncmp(json, &token[key_index], "id", 2)) {
-      log_debug("id found\n");
-
-      if (!is_valid_nostr_event_id(&funcs, json, &token[value_index])) {
-        return false;
-      }
-
-      continue;
-    }
-
-    if (funcs.strncmp(json, &token[key_index], "pubkey", 6)) {
-      log_debug("pubkey found\n");
-
-      if (!is_valid_nostr_event_pubkey(&funcs, json, &token[value_index])) {
-        return false;
-      }
-
-      continue;
-    }
-
-    if (funcs.strncmp(json, &token[key_index], "kind", 4)) {
-      log_debug("kind found\n");
-
-      if (!is_valid_nostr_event_kind(&funcs, json, &token[value_index])) {
-        return false;
-      }
-
-      continue;
-    }
-
-    if (funcs.strncmp(json, &token[key_index], "created_at", 10)) {
-      log_debug("created_at found\n");
-
-      if (!is_valid_nostr_event_created_at(&funcs, json, &token[value_index])) {
-        return false;
-      }
-
-      continue;
-    }
-
-    if (funcs.strncmp(json, &token[key_index], "sig", 3)) {
-      log_debug("sig found\n");
-
-      if (!is_valid_nostr_event_sig(&funcs, json, &token[value_index])) {
-        return false;
-      }
-
-      continue;
-    }
-
-    if (funcs.strncmp(json, &token[key_index], "tags", 4)) {
-      log_debug("tags found\n");
-
-      if (!is_valid_nostr_event_tags(&funcs, json, &token[value_index])) {
-        return false;
-      }
-
-      continue;
-    }
+  if (!is_valid_nostr_event(&funcs, json, &token[3], token_count - 3)) {
+    return false;
   }
 
   return true;
