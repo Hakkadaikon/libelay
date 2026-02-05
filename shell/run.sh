78dcda694ac40930a1f2091ee95ad799f8763158
hakkadaikon <hakkadaikon@yahoo.co.jp>
2026-02-06 02:52:19 +0900

    fix: Fix run.sh

--- shell/run.sh
+++ shell/run.sh
@@ -2,51 +2,51 @@
 
 cd $(dirname $0)
 
-ECHOBACK_SERVER_PATH=../examples/echoback
-ECHOBACK_SERVER_BIN_PATH=${ECHOBACK_SERVER_PATH}/bin/wsserver
+BUILD_PATH=../
+RELAY_BIN_PATH=${BUILD_PATH}/build/libelay
 
 clean() {
   rm -rf ../build 1>/dev/null
-  make clean -C ${ECHOBACK_SERVER_PATH} 1>/dev/null
+  make clean -C ${BUILD_PATH} 1>/dev/null
 }
 
 wsDebugBuild() {
   rm -rf ../build 1>/dev/null
-  make clean -C ${ECHOBACK_SERVER_PATH} 1>/dev/null
+  make clean -C ${BUILD_PATH} 1>/dev/null
   cmake -S .. -B ../build -DCMAKE_BUILD_TYPE=Debug 1>/dev/null
   cmake --build ../build
-  BUILD=debug make -C ${ECHOBACK_SERVER_PATH}
+  BUILD=debug make -C ${BUILD_PATH}
 }
 
 wsReleaseBuild() {
   rm -rf ../build 1>/dev/null
-  make clean -C ${ECHOBACK_SERVER_PATH} 1>/dev/null
+  make clean -C ${BUILD_PATH} 1>/dev/null
   cmake -S .. -B ../build -DCMAKE_BUILD_TYPE=Release 1>/dev/null
   cmake --build ../build
-  BUILD=release make -C ${ECHOBACK_SERVER_PATH}
+  BUILD=release make -C ${BUILD_PATH}
 }
 
 case "$1" in
   memcheck)
     wsDebugBuild
     echo "debug run (memcheck)"
-    valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes -s ${ECHOBACK_SERVER_BIN_PATH}
+    valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all --track-origins=yes -s ${RELAY_BIN_PATH}
     ;;
   helgrind)
     wsDebugBuild
     echo "debug run (helgrind)"
-    valgrind --tool=helgrind --history-level=approx -s ${ECHOBACK_SERVER_BIN_PATH}
+    valgrind --tool=helgrind --history-level=approx -s ${RELAY_BIN_PATH}
     ;;
   debug)
     wsDebugBuild
     echo "debug run"
-    #gdb --args ${ECHOBACK_SERVER_BIN_PATH}
-    ${ECHOBACK_SERVER_BIN_PATH}
+    #gdb --args ${RELAY_BIN_PATH}
+    ${RELAY_BIN_PATH}
     ;;
   release)
     wsReleaseBuild
     echo "release run"
-    ${ECHOBACK_SERVER_BIN_PATH}
+    ${RELAY_BIN_PATH}
     ;;
   clean)
     clean
@@ -54,6 +54,6 @@ case "$1" in
   *)
     wsReleaseBuild
     echo "release run"
-    ${ECHOBACK_SERVER_BIN_PATH}
+    ${RELAY_BIN_PATH}
     ;;
 esac
